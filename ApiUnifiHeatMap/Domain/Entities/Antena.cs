0fe34bfc39bd35d742d456e4dbb49604bfd760e8
RobsonTrasel <robson.trasel@gmail.com>
2023-07-02 23:19:14 -0300

    chore(entity): create a new entity for antena

---
+++ ApiUnifiHeatMap/Domain/Entities/Antena.cs
@@ -0,0 +1,12 @@
+﻿using ApiUnifiHeatMap.Domain.ValueObjects;
+
+namespace ApiUnifiHeatMap.Domain.Entities;
+
+public class Antena
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public Coordenadas Coordenadas { get; set; }
+    
+    public ICollection<LeituraSinal> LeituraSinal { get; set; }
+}
