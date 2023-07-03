0e1e2db51b12702051f1ccbbf4c658b0c5bc0e1f
RobsonTrasel <robson.trasel@gmail.com>
2023-07-03 00:05:31 -0300

    chore(interface): add a interface for leituraSinal

---
+++ ApiUnifiHeatMap/Domain/Interfaces/Repositories/ILeituraDeSinalRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiUnifiHeatMap.Domain.Entities;
+
+namespace ApiUnifiHeatMap.Domain.Interfaces.Repositories;
+
+public interface ILeituraDeSinalRepository
+{
+    Task<IEnumerable<LeituraSinal>> GetAllLeiturasSinalAsync();
+    Task<IEnumerable<LeituraSinal>> GetLeiturasSinalByAntenaIdAsync(int antenaId);
+    Task CreateLeituraSinalAsync(LeituraSinal leituraSinal);
+}
