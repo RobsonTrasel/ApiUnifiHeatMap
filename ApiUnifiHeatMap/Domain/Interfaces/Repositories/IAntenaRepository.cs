8fc26978a1195b550f230030ac2ab3e92b72052c
RobsonTrasel <robson.trasel@gmail.com>
2023-07-03 00:05:47 -0300

    chore(interface): add a new interface for antena repository

---
+++ ApiUnifiHeatMap/Domain/Interfaces/Repositories/IAntenaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiUnifiHeatMap.Domain.Entities;
+
+namespace ApiUnifiHeatMap.Domain.Interfaces.Repositories;
+
+public interface IAntenaRepository
+{
+    Task<IEnumerable<Antena>> GetAllAntenasAsync();
+    Task<Antena> GetAntenaByIdAsync(int id);
+    Task CreateAntenaAsync(Antena antena);
+    Task UpdateAntenaAsync(Antena antena);
+    Task DeleteAntenaAsync(int id);
+}
