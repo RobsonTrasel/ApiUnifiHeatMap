f8fc37246a3228bf436fa7f217c18c23f5b8a62f
RobsonTrasel <robson.trasel@gmail.com>
2023-07-03 00:05:09 -0300

    chore(interface): add a interface for sinal

---
+++ ApiUnifiHeatMap/Domain/Interfaces/Service/ISinalService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiUnifiHeatMap.Domain.Entities;
+
+namespace ApiUnifiHeatMap.Domain.Interfaces.Service;
+
+public interface ISinalService
+{
+    Task<IEnumerable<Antena>> GetAllAntenasAsync();
+    Task<IEnumerable<LeituraSinal>> GetLeiturasSinalByAntenaIdAsync(int antenaId);
+    Task AddLeituraSinalAsync(int antenaId, int sinal);   
+}
