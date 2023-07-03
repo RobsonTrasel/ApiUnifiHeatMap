0bddb4c82762ca4222da3d7d28c2f18ac4fe5391
RobsonTrasel <robson.trasel@gmail.com>
2023-07-02 23:19:35 -0300

    chore(entity): create a new entity for read antena signal

---
+++ ApiUnifiHeatMap/Domain/Entities/LeituraSinal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ApiUnifiHeatMap.Domain.Entities;
+
+public class LeituraSinal
+{
+    public int Id { get; set; }
+    public DateTime DataLeitura { get; set; }
+    public int Sinal { get; set; }
+    
+    public int AntenaId { get; set; }
+    public Antena Antena { get; set; }
+}
